24b0a7bb70037e9c26ecbc6bab72770e62f1873a
DurayBence <73463885+DurayBence@users.noreply.github.com>
2024-09-24 18:06:25 +0200

    Update test_mymath.py
    intentionally break test_add_string__expect_exception()

--- test_mymath.py
+++ test_mymath.py
@@ -12,5 +12,5 @@ def test_add_negative():
     assert add_numbers(4, -100) == -96
 
 def test_add_string__expect_exception():
-    with pytest.raises(TypeError):
-        add_numbers(4, 'I DO NOT BELONG HERE')
+    # with pytest.raises(TypeError):
+    assert add_numbers(4, 'I DO NOT BELONG HERE') == 4
